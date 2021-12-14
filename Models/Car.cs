1adddfe3636194efedab32174a5187fc218609b0
Kate Kiatsiri <keidsiri@gmail.com>
2021-12-14 14:01:31 -0800

    Initialize folders and file
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>

---
+++ Models/Car.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace Dealership.Models
+{
+  public class Car
+  {
+    // Public auto-implemented properties
+    public string MakeModel { get; set; }
+    public int Price { get; set; }
+    public int Miles { get; set; }
+
+    //Constructor
+    public Car(string makeModel, int price, int miles)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+    }
+
+    public string GetMakeModel()
+    {
+      return MakeModel;
+    }
+    
+    public void SetPrice(int newPrice)
+    {
+      Price = newPrice;
+    }
+    
+    public int GetPrice()
+    {
+      return Price;
+    }
+
+    public int GetMiles()
+    {
+      return Miles;
+    }
+
+    public bool WorthBuying(int maxPrice)
+    {
+      return (Price <= maxPrice);
+    }
+  }
+}
